fff0fde66febbce783d991c08ee0d276bd911cbc
Derek Feichtinger <derek.feichtinger@psi.ch>
2021-01-02 22:49:39 +0100

    Fix for popping up frame containing mu headers
    When selecting a mu4e link, I want that the frame containing my email
    windows is showing the email. This fix choodes the frame showing the
    headers buffer.

--- mu4e/mu4e-headers.el
+++ mu4e/mu4e-headers.el
@@ -773,9 +773,12 @@ after the end of the search results."
       (when mu4e~headers-msgid-target
         (if (eq (current-buffer) (window-buffer))
             (mu4e-headers-goto-message-id mu4e~headers-msgid-target)
-          (let* ((pos (mu4e-headers-goto-message-id mu4e~headers-msgid-target)))
+          (let* ((pos (mu4e-headers-goto-message-id mu4e~headers-msgid-target))
+                 (win (get-buffer-window (mu4e-get-headers-buffer) 0)))
             (when pos
-              (set-window-point (get-buffer-window) pos)))))
+	      (select-frame-set-input-focus (window-frame win))
+	      (select-window win)
+	      (goto-char pos)))))
       (when (and mu4e~headers-view-target (mu4e-message-at-point 'noerror))
         ;; view the message at point when there is one.
         (mu4e-headers-view-message))
